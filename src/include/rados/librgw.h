723f4702462b15e0d734912c3fae55b7ea63ee75
Matt Benjamin <mbenjamin@redhat.com>
2015-08-28 16:11:49 -0400

    librgw: install librgw.h
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/include/rados/librgw.h
+++ src/include/rados/librgw.h
@@ -18,8 +18,8 @@
 extern "C" {
 #endif
 
-class CephContext;
-typedef CephContext* librgw_t;
+//class CephContext;
+typedef void* librgw_t;
 int librgw_create(librgw_t* rgw, const char* const id);
 int librgw_acl_bin2xml(librgw_t rgw, const char* bin, int bin_len, char** xml);
 void librgw_free_xml(librgw_t rgw, char* xml);

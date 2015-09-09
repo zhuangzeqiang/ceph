05eb92b3edfb8be5275e26c059477fea5d7addd9
Yan, Zheng <zyan@redhat.com>
2015-09-09 14:14:33 +0800

    client: use faked inode number when sizeof(ino_t) < 8
    Cephfs uses 48-bits inode number. When ino_t is 32-bits, the upper 16
    bits of inode number gets lost for stat(2) and readdir(2).
    
    32-bits ino_t is even more problematic with ceph-fuse. In ceph-fuse
    case, we need to encode both cephfs inode number and snapid into
    fuse_ino_t. 32-bits are clearly not enough.
    
    The fix is assign a 32-bits faked inode number to each cached inode.
    Faked inode numbers are used for stat(2), readdir(2) and fuse. We
    maintain a data structure that maps faked inode number to cephfs's
    vinodeno_t. So we can find inode by the 32-bits faked inode number.
    This appoach should work as long as there are less than 4 billions
    cached inodes.
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/client/Inode.h
+++ src/client/Inode.h
@@ -159,6 +159,10 @@ struct Inode {
   // -- the actual inode --
   inodeno_t ino;
   snapid_t  snapid;
+#if SIZEOF_INO_T < 8
+  ino_t faked_ino;
+#endif
+
   uint32_t   rdev;    // if special file
 
   // affected by any inode change...
@@ -297,6 +301,9 @@ struct Inode {
 
   Inode(Client *c, vinodeno_t vino, ceph_file_layout *newlayout)
     : client(c), ino(vino.ino), snapid(vino.snapid),
+#if SIZEOF_INO_T < 8
+      faked_ino(0),
+#endif
       rdev(0), mode(0), uid(0), gid(0), nlink(0),
       size(0), truncate_seq(1), truncate_size(-1),
       time_warp_seq(0), max_size(0), version(0), xattr_version(0),
